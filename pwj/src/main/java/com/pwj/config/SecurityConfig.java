558406f56db385edcd454dd35050ccc2e8469dd3
junye <junye@DESKTOP-EEPHA35.kornet>
2025-02-11 21:04:51 +0900

    csrf

--- pwj/src/main/java/com/pwj/config/SecurityConfig.java
+++ pwj/src/main/java/com/pwj/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -13,8 +14,8 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests(auth -> auth.requestMatchers("/", "/login").permitAll().anyRequest().authenticated())
-				.formLogin(formLogin -> formLogin.loginPage("/login").defaultSuccessUrl("/"));
+				.formLogin(formLogin -> formLogin.loginPage("/login").defaultSuccessUrl("/"))
+				.logout(logout->logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").invalidateHttpSession(true));
 		return http.build();
 	}
-
 }
